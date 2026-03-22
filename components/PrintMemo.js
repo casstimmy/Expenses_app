7439c0db385dd9610047cf84ee50d980d5caa878
Ayoola <47798700+casstimmy@users.noreply.github.com>
2026-03-22 20:50:22 +0100

    improve on the webproduct optimization and ux

--- components/PrintMemo.js
+++ components/PrintMemo.js
@@ -218,7 +218,7 @@ const PrintMemo = forwardRef(
                 </span>
               </p>
               <p style={{ fontWeight: "bold", paddingTop: "3.5em" }}>
-                Catherine Ashenuga-Farrer
+                Paul Farrer
               </p>
               <p style={{ fontWeight: "bold" }}>Director</p>
             </div>
